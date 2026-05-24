db6e04b2c9aa6a03d320175090b170a54cd06183
unknown <franzaaronseballos@gmail.com>
2026-05-24 21:24:40 +0800

    testinggggggggggggg

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 
 class CreateController extends Controller
 {
-<<<<<<< HEAD
-    //  ahdabcasbajdwqe
-=======
-    //test test testttt lei
->>>>>>> 324bfd8f54c0705c9e5a71c288438d3cf834acde
+
+    //  qweqdasdqeqweqw
 }
